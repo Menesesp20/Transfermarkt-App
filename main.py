a1d527b7264292ecc71f3ca3e6a45455659b613b
Menesesp20 <77457105+Menesesp20@users.noreply.github.com>
2024-08-05 12:42:27 +0100

    Update Percentile Value
    Return Average of the player

--- main.py
+++ main.py
@@ -363,7 +363,7 @@ if st.button('Get Data'):
 
                 merged_df = merged_df[columns_order]
 
-                Percentile = int(merged_df['Percentile'].unique()[0])
+                Percentile = int(merged_df['Percentile'].mean())
                 mls_Grade = str(merged_df['Grade_MLS'].unique()[0])
                 minutes_Percentage = str(merged_df['Minutes Percentage'].unique()[0])
 
